684efe0a307f6af367e22ce45414f9eec39a5da2
Candra Sidik Dermawan <projectcsd07@gmail.com>
2024-01-04 15:21:59 +0700

    disable eslint

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -30,7 +30,7 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
                             width={20}
                             height={20}
                             className='ml-4'
-                            alt='car logo&quot;'
+                            alt='car logo'
                         />
                     </Combobox.Button>
 
